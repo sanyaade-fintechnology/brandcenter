3ffeaff0a8385f4bdab063a76b3baa6374672670
Andrei Balcanasu <andrei.balcanasu@sumup.com>
2013-10-25 11:46:55 +0100

    Set the last updated date from the status json

--- js/main.js
+++ js/main.js
@@ -8,6 +8,8 @@ $.ajax({
   dataType: "json",
   success: function (data) {
     statusUpdatedAt = new Date(data["updated_at"]);
+    $(".date").html(statusUpdatedAt.toLocaleDateString())
+    $(".time").html(statusUpdatedAt.toLocaleTimeString())
     services = data["services"];
     for (service in services) {
       if (services.hasOwnProperty(service)){
